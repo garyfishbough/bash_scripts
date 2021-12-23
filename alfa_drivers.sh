efa10fdac1f8af855febd6e12f57c18d119933a6
elfish <41020376+garyfishbough@users.noreply.github.com>
2021-12-22 16:45:16 -0800

    Add files via upload

---
+++ alfa_drivers.sh
@@ -0,0 +1,14 @@
+
+sudo apt install realtek-rtl88xxau-dkms
+sudo apt install dkms
+git clone https://github.com/aircrack-ng/rtl8812au
+cd rtl8812au/
+make
+sudo make install
+sudo wifite 
+
+
+
+
+
+
